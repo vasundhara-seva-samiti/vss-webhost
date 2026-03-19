98f9ec9ad67df5f55e6497cced2c8d880553ba4b
vasundhara-seva-samiti <peparambarupal@gmail.com>
2026-03-19 12:22:53 +0530

    add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vss-webhost/static-site',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b3a27ca4eee88db4e10dd2a0cc88871314dd2493',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vss-webhost/static-site' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b3a27ca4eee88db4e10dd2a0cc88871314dd2493',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
